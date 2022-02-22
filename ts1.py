8d7b78fecd52f2b50eb5b936828e3528caa47f88
Jeevan Vasanthan <jeevan.vasanthan@gmail.com>
2022-02-22 15:48:02 -0500

    ts1 update

---
+++ ts1.py
@@ -0,0 +1,8 @@
+#Rough idea:
+# read data from text file and store ip address of each domain name in  python dictionary
+#set up socket configurations for ts1 server
+#listen for connection
+#accept connection
+# recieve data from root server
+#search for domain name in dictionary and send back ip address if found. Otherwise, give no response.
+
