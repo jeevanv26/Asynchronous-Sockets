e0112de3b371c0dd682d03aa9107a7a9ba159dda
HasinJ <hasinchoudhuryj@gmail.com>
2022-02-22 20:26:20 -0500

    collects data from client, sends to TS1 or TS2 as they're available

--- rs.py
+++ rs.py
@@ -6,91 +6,71 @@ import select
 
 def server():
     try:
-<<<<<<< HEAD
         rs = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-=======
-        rs= socket.socket(socket.AF_INET, socket.SOCK_STREAM)
->>>>>>> 2057a2f6fec62689d6a5b69e7edbe3a27559fa00
-        TS1 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        TS2 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         print("[RS]: Server socket created")
     except socket.error as err:
         print('socket open error: {}\n'.format(err))
         exit()
 
-    # Define the port on which you want to connect to the server
     TS1port = 50011
     TS1host_addr = socket.gethostbyname(socket.gethostname()) #local host name
     TS1_binding = (TS1host_addr, TS1port)
-<<<<<<< HEAD
     TS1.connect(TS1_binding)
-=======
-    rs.connect(TS1_binding)
->>>>>>> 2057a2f6fec62689d6a5b69e7edbe3a27559fa00
 
     TS2port = 50012
     TS2host_addr = socket.gethostbyname(socket.gethostname()) #local host name
     TS2_binding = (TS2host_addr, TS2port)
-    rs.connect(TS2_binding)
+    TS2.connect(TS2_binding)
 
-
-<<<<<<< HEAD
-    server_binding = ('', 50010) #for the client
-=======
     server_binding = ('', 50010)
->>>>>>> 2057a2f6fec62689d6a5b69e7edbe3a27559fa00
     rs.setblocking(0)
     rs.bind(server_binding)
     rs.listen(2)
     host = socket.gethostname() #local host name
     localhost_ip = (socket.gethostbyname(host))
-    csockid, addr = rs.accept()
+    csockid, addr = ss.accept()
 
     print("[RS]: Server host name is {}".format(host))
     print("[RS]: Server IP address is {}".format(localhost_ip))
     print ("[RS]: Got a connection request from a client at {}".format(addr))
 
-    alldata = []
 
+    alldata = [] #from client
     while(True):
         data = csockid.recv(1024)
         if not data: break
         print("[RS]: received data from client")
-<<<<<<< HEAD
         alldata.append(data)
-
     print(alldata)
-
 
     inputs = [TS1, TS2]
     outputs = []
     messages = {}
+    counter = 0
 
     while inputs:
         readable, writable, errors = select.select(inputs, outputs, [], 5)
 
         if readable or writable
             for r in readable:
-                if r is TS1 or TS2:
+                if r is TS1 or TS2: #accept all sockets
                     conn, add = r.accept()
                     conn.setblocking(0)
                     input.append(conn)
-                else:
+                else: #data gets thrown into RS to read
                     data = r.recv(200)
                     outputs.append(r)
-                    messages[r] = 'test'
+                    print("[S]: Server got {} from TS".format(data))
+                    messages[r] = data
                     inputs.remove(r)
 
-            for w in writable:
-                msg = messages[w]
+            for w in writable: #
+                msg = alldata[counter]
                 w.send(msg)
                 outputs.remove(w)
+                counter+=1
 
         else:
-            ss.close()
+            rs.close()
             input.remove(TS1)
             input.remove(TS2)
-=======
-        rs.send(data.encode())
-        print("[RS]: sent data to servers")
->>>>>>> 2057a2f6fec62689d6a5b69e7edbe3a27559fa00
