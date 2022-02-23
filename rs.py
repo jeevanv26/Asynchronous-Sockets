df430b38d0e6e4e231a58e9c10926757e5e92b3e
HasinJ <hasinchoudhuryj@gmail.com>
2022-02-22 19:14:42 -0500

    need to send data to TS1 and TS1 first and then receive responses

--- rs.py
+++ rs.py
@@ -6,63 +6,22 @@ import select
 
 def server():
     try:
-<<<<<<< HEAD
         rs = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-=======
-        rs= socket.socket(socket.AF_INET, socket.SOCK_STREAM)
->>>>>>> 2057a2f6fec62689d6a5b69e7edbe3a27559fa00
-        TS1 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        TS2 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         print("[RS]: Server socket created")
     except socket.error as err:
         print('socket open error: {}\n'.format(err))
         exit()
 
-    # Define the port on which you want to connect to the server
-    TS1port = 50011
-    TS1host_addr = socket.gethostbyname(socket.gethostname()) #local host name
-    TS1_binding = (TS1host_addr, TS1port)
-<<<<<<< HEAD
-    TS1.connect(TS1_binding)
-=======
-    rs.connect(TS1_binding)
->>>>>>> 2057a2f6fec62689d6a5b69e7edbe3a27559fa00
-
-    TS2port = 50012
-    TS2host_addr = socket.gethostbyname(socket.gethostname()) #local host name
-    TS2_binding = (TS2host_addr, TS2port)
-    rs.connect(TS2_binding)
-
-
-<<<<<<< HEAD
-    server_binding = ('', 50010) #for the client
-=======
-    server_binding = ('', 50010)
->>>>>>> 2057a2f6fec62689d6a5b69e7edbe3a27559fa00
     rs.setblocking(0)
-    rs.bind(server_binding)
-    rs.listen(2)
-    host = socket.gethostname() #local host name
-    localhost_ip = (socket.gethostbyname(host))
-    csockid, addr = rs.accept()
 
-    print("[RS]: Server host name is {}".format(host))
-    print("[RS]: Server IP address is {}".format(localhost_ip))
-    print ("[RS]: Got a connection request from a client at {}".format(addr))
+    # Bind the socket to the port
+    rs = ('', 50015)
+    server.bind(server_address)
 
-    alldata = []
+    # Listen for incoming connections
+    server.listen(5)
 
-    while(True):
-        data = csockid.recv(1024)
-        if not data: break
-        print("[RS]: received data from client")
-<<<<<<< HEAD
-        alldata.append(data)
-
-    print(alldata)
-
-
-    inputs = [TS1, TS2]
+    inputs = [rs]
     outputs = []
     messages = {}
 
@@ -71,14 +30,14 @@ def server():
 
         if readable or writable
             for r in readable:
-                if r is TS1 or TS2:
+                if r is rs:
                     conn, add = r.accept()
                     conn.setblocking(0)
                     input.append(conn)
                 else:
                     data = r.recv(200)
                     outputs.append(r)
-                    messages[r] = 'test'
+                    messages[r] = "Successful"
                     inputs.remove(r)
 
             for w in writable:
@@ -87,10 +46,6 @@ def server():
                 outputs.remove(w)
 
         else:
-            ss.close()
+            rs.close()
             input.remove(TS1)
             input.remove(TS2)
-=======
-        rs.send(data.encode())
-        print("[RS]: sent data to servers")
->>>>>>> 2057a2f6fec62689d6a5b69e7edbe3a27559fa00
